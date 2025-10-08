b1464966baf4fe4870c706bf0480e42ada1603ca
NgrimaldiN <nicolas.grimaldi2@gmail.com>
2025-10-08 19:01:08 +0200

    ajout du storage des urls

--- scripts/speech_url_scrap.py
+++ scripts/speech_url_scrap.py
@@ -1,9 +1,12 @@
+import sys,os
+sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..','src')))
 import requests
 import time
 from rollcall.browser_setup import get_browser
 from rollcall.speech_filter_action import open_page_close_popup_and_click_filters
 from rollcall.scroller import scroll_to_bottom
 from rollcall.url_extractor import extract_urls
+from rollcall.storage import store_urls
 
 def main():
     browser = get_browser()
@@ -11,7 +14,7 @@ def main():
     open_page_close_popup_and_click_filters(browser, url)
     scroll_to_bottom(browser)
     urls = extract_urls(browser)
-    print(urls)
+    store_urls(urls)
 
 if __name__ == "__main__":
     main()
--- src/rollcall/browser_setup.py
+++ src/rollcall/browser_setup.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 
 def get_browser():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=1920,1080")
