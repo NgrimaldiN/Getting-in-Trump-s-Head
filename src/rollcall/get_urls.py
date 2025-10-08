b430f76cadf885e46d5bc23a980fd00959d21b76
NgrimaldiN <nicolas.grimaldi2@gmail.com>
2025-10-08 16:30:32 +0200

    Nouveau fichier pour récupérer tous les URL : En utilisant Selenium, on récupère la page filtrée pour Speech

---
+++ src/rollcall/get_urls.py
@@ -0,0 +1,29 @@
+import requests
+from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_experimental_option("detach", True)
+path_to_web_driver = chromedriver_autoinstaller.install()
+service = Service(executable_path=path_to_web_driver)
+browser = webdriver.Chrome(service=service,
+                           options=chrome_options)
+
+    
+
+url='https://rollcall.com/factbase/trump/search/'
+
+browser.get(url)
+
+close_btn = browser.find_element(By.CSS_SELECTOR, "div.cursor-pointer.text-right.mr-4.mt-2")
+close_btn.click()
+
+label = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "label[for='Speech']")))
+label.click()
