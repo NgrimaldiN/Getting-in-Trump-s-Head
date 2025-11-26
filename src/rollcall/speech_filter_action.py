7f7374e17d24cad8bda81dc339f0097927c72906
NgrimaldiN <nicolas.grimaldi2@gmail.com>
2025-11-26 19:05:35 +0100

    adding an exception rule since we don t always have the popup

--- src/rollcall/speech_filter_action.py
+++ src/rollcall/speech_filter_action.py
@@ -5,10 +5,13 @@ import time
 
 def open_page_close_popup_and_click_filters(browser, url):
     browser.get(url)
-    close_btn=WebDriverWait(browser, 5).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cursor-pointer.text-right.mr-4.mt-2"))
-        )
-    close_btn.click()
+    try :
+        close_btn=WebDriverWait(browser, 5).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cursor-pointer.text-right.mr-4.mt-2"))
+            )
+        close_btn.click()
+    except :
+        pass
     label = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "label[for='Speech']")))
     label.click()
     time.sleep(2)
