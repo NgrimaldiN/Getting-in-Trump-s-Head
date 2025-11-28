a5c270e5ae47c922653d77c15a36639b9f9067ce
NgrimaldiN <nicolas.grimaldi2@gmail.com>
2025-11-28 14:48:13 +0100

    correction bug

--- src/rollcall/speech_filter_action.py
+++ src/rollcall/speech_filter_action.py
@@ -2,13 +2,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
+from selenium.common.exceptions import TimeoutException
 
 def open_page_close_popup_and_click_filters(browser, url):
     browser.get(url)
-    close_btn=WebDriverWait(browser, 5).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cursor-pointer.text-right.mr-4.mt-2"))
-        )
-    close_btn.click()
+    try:
+        close_btn=WebDriverWait(browser, 5).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cursor-pointer.text-right.mr-4.mt-2"))
+            )
+        close_btn.click()
+    except TimeoutException:
+        pass
     label = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "label[for='Speech']")))
     label.click()
     time.sleep(2)
